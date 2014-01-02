2a212a1109fb203d4928233d6cbb5c666756b4c7
RouLeTaMiX <rouletamix@gmail.com>
2014-01-02 23:35:55 +0100

    Change var name on main.js file.

--- assets/main.js
+++ assets/main.js
@@ -3,21 +3,23 @@
  */
 
 /* init some variables */
-var word = "ToXTa-MiXTa";
+var myName = "ToxTa-MiXTa";
 
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var colorPolicy = [purple, blue, green, orange, red];
+var letterColors = [red, orange, green, blue, purple];
 
-drawName(word, colorPolicy);
+drawName(myName, letterColors);
 
-if (10 < 3){
+if(10 < 3)
+{
     bubbleShape = 'square';
 }
-else{
+else
+{
     bubbleShape = 'circle';
 }
 
