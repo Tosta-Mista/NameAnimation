c706feef6eab82e4cf74da95c3f7b96884ab915b
RouLeTaMiX <rouletamix@gmail.com>
2014-01-02 23:48:56 +0100

    Fix: some wrong code must to be fixed!

--- assets/main.js
+++ assets/main.js
@@ -12,15 +12,12 @@ var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 var letterColors = [red, orange, green, blue, purple];
 
+if(10>3){
+    bubbleShape = "circle";
+}
+else{
+    bubbleShape = "square";
+}
+
+bounceName();
 drawName(myName, letterColors);
-
-if(10 < 3)
-{
-    bubbleShape = 'square';
-}
-else
-{
-    bubbleShape = 'circle';
-}
-
-bounceBubbles();
