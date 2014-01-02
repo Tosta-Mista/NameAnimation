c310ace960bfbdeba52e00bd595ffcae1e235172
RouLeTaMiX <rouletamix@gmail.com>
2014-01-02 23:50:29 +0100

    Change: name of variables

--- assets/main.js
+++ assets/main.js
@@ -3,16 +3,15 @@
  */
 
 /* init some variables */
-var myName = "ToxTa-MiXTa";
-
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
 
-if(10>3){
+var myName = "ToXTa-MiXTa";
+letterColors = [red, orange, green, blue, purple];
+if (10 > 3) {
     bubbleShape = "circle";
 }
 else{
