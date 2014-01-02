3c3a167b3f77d107986c660c7b7f5d64e6d7dc02
RouLeTaMiX <rouletamix@gmail.com>
2014-01-02 23:26:46 +0100

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jgo on 02.01.14.
+ */
+
+/* init some variables */
+var word = "José Gonçalves";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var colorPolicy = [purple, blue, green, orange, red];
+
+drawName(word, colorPolicy);
+
+if (10 < 3){
+    bubbleShape = 'square';
+}
+else{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
